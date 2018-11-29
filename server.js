da36f9d3a51c34e0ca677cd9705c9325b0a901e3
kartimanu <karthik.manjunath@kaplan.com>
2018-11-29 13:52:49 +0530

    origin fix - 0.3

--- server.js
+++ server.js
@@ -27,9 +27,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
+    // res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 });
 // app.use(function (req, res, next) {
 //     if (req.url !== '/authUser' && req.url !== "/") {
@@ -55,11 +65,11 @@ app.use(function(req,res,next){
 var port = process.env.port || 8080;
 // var router = express.Router();
 
-app.get("/", function (req, res) { res.send("[ Home - Page of API's (V1.1.9)]") });
+app.get("/", function (req, res) { res.send("[ Home - Page of API's (V1.1.9) ]") });
 
-app.get("/getDCreportbyMonth", reportDCfunc.report.getdailycount);
-app.get("/getDCreportbyday", reportDCfunc.report.getdailycountbyday);
-app.post("/getDCreportbyrange", reportDCfunc.report.getdailycountbyrange);
+app.get("/getDCreportbyMonth", cor(), reportDCfunc.report.getdailycount);
+app.get("/getDCreportbyday", cor(), reportDCfunc.report.getdailycountbyday);
+app.post("/getDCreportbyrange", cor(), reportDCfunc.report.getdailycountbyrange);
 
 app.get("/getHWCreport_byCat", cors(), reportHWCfunc.report.getHWC_byCat);
 app.get("/getHWCreport_bycases", cors(), reportHWCfunc.report.getHWC_caseattended);
@@ -74,41 +84,41 @@ app.post("/createuser", userfunctions.caller.createUser);
 app.get("/deleteuser/:id", userfunctions.caller.deleteUser);
 app.post("/updateuser", userfunctions.caller.updateUser);
 
-app.get("/getDailyCountAO", dcfunctions.caller.getdailyDAO);
-app.get("/getDailyCountFA/:id", dcfunctions.caller.getFAusers);
-app.get("/getallDC", dcfunctions.caller.getDailyCount);
+app.get("/getDailyCountAO", cor(), dcfunctions.caller.getdailyDAO);
+app.get("/getDailyCountFA/:id", cor(), dcfunctions.caller.getFAusers);
+app.get("/getallDC", cor(), dcfunctions.caller.getDailyCount);
 
-app.get("/getCompensation_OM", comfunctions.caller.getOMdetails);
-app.get("/getOM_cases/:id", comfunctions.caller.getOM_casedetails);
+app.get("/getCompensation_OM", cor(), comfunctions.caller.getOMdetails);
+app.get("/getOM_cases/:id", cor(), comfunctions.caller.getOM_casedetails);
 
-app.get("/getpublicity", pubfunctions.caller.getpubdata);
+app.get("/getpublicity", cor(), pubfunctions.caller.getpubdata);
 
-app.get("/getallhwc", hwcfunctions.caller.get_all_hwc);
-app.get("/gethwc", hwcfunctions.caller.get_hwc);
-app.get("/gethwc/:id", hwcfunctions.caller.get_hwcall_byid);
+app.get("/getallhwc", cor(), hwcfunctions.caller.get_all_hwc);
+app.get("/gethwc", cor(), hwcfunctions.caller.get_hwc);
+app.get("/gethwc/:id", cor(), hwcfunctions.caller.get_hwcall_byid);
 
-app.get("/getcase_users", csfunctions.caller.get_case_users);
-app.get("/img", pubfunctions.caller.getpubImg);
+app.get("/getcase_users", cor(), csfunctions.caller.get_case_users);
+app.get("/img", cor(), pubfunctions.caller.getpubImg);
 
-app.get("/getDuplicateRecord/:id",errfunctions.caller.get_hwcDuplicateData);
-app.get("/getParentRecord/:id",errfunctions.caller.get_hwcParentData);
-app.get("/getErrorRecords",errfunctions.caller.get_errorRecords);
-app.post("/updateParentRecord",errfunctions.caller.update_hwcParentData);
-app.get("/updateErrorRecord/:id",errfunctions.caller.update_errorRecord);
-app.get("/insertErrorRecord/:id",hwcSyncfunc.func.setDupRecordDetails);
+app.get("/getDuplicateRecord/:id", cor(), errfunctions.caller.get_hwcDuplicateData);
+app.get("/getParentRecord/:id", cor(), errfunctions.caller.get_hwcParentData);
+app.get("/getErrorRecords", cor(), errfunctions.caller.get_errorRecords);
+app.post("/updateParentRecord", cor(), errfunctions.caller.update_hwcParentData);
+app.get("/updateErrorRecord/:id", cor(), errfunctions.caller.update_errorRecord);
+app.get("/insertErrorRecord/:id", cor(), hwcSyncfunc.func.setDupRecordDetails);
 
 
 //home
 
-app.get("/getTotalCasesbyYear", dash_chart_homefunc.report.getTotalCasesByYear);
-app.get("/getCategorybyYear", dash_chart_homefunc.report.getCategoryByYear);
-app.post("/getBpNhByRange", dash_chart_homefunc.report.getBpNhByRange);
-app.post("/getBpNhByRange", dash_chart_homefunc.report.getBpNhByRange);
-app.get("/getPreviousBpNhCount", dash_chart_homefunc.report.getPreviousBpNhCount);
-app.post("/getBpByCategory", dash_chart_homefunc.report.getBpByCategory);
-app.post("/getNhByCategory", dash_chart_homefunc.report.getNhByCategory);
-app.post("/getBpNhByCategory", dash_chart_homefunc.report.getBpNhByCategory);
-app.get("/getBpNhYearly", dash_chart_homefunc.report.getBpNhYearly);
+app.get("/getTotalCasesbyYear", cor(), dash_chart_homefunc.report.getTotalCasesByYear);
+app.get("/getCategorybyYear", cor(), dash_chart_homefunc.report.getCategoryByYear);
+app.post("/getBpNhByRange", cor(), dash_chart_homefunc.report.getBpNhByRange);
+app.post("/getBpNhByRange", cor(), dash_chart_homefunc.report.getBpNhByRange);
+app.get("/getPreviousBpNhCount", cor(), dash_chart_homefunc.report.getPreviousBpNhCount);
+app.post("/getBpByCategory", cor(), dash_chart_homefunc.report.getBpByCategory);
+app.post("/getNhByCategory", cor(), dash_chart_homefunc.report.getNhByCategory);
+app.post("/getBpNhByCategory", cor(), dash_chart_homefunc.report.getBpNhByCategory);
+app.get("/getBpNhYearly", cor(), dash_chart_homefunc.report.getBpNhYearly);
 
 //HWC
 app.get("/getblock1", cors(), dash_chartfunc.report.getHWC_block1);
@@ -122,18 +132,18 @@ app.get("/getblock2_top50cases_bywsid", cors(), dash_chartfunc.report.get_top50c
 app.get("/getblock3_topcases", cors(), dash_chartfunc.report.get_topfreq_block3);
 
 //Compensation
-app.get("/gettotalcomp",dash_chart_compfunc.report.get_total_comp);
-app.post("/getcomp_filter",dash_chart_compfunc.report.get_comp_block12_bydate);
-app.post("/get_top_comp",dash_chart_compfunc.report.get_comp_topwsid_topvillage);
+app.get("/gettotalcomp", cor(), dash_chart_compfunc.report.get_total_comp);
+app.post("/getcomp_filter", cor(), dash_chart_compfunc.report.get_comp_block12_bydate);
+app.post("/get_top_comp", cor(), dash_chart_compfunc.report.get_comp_topwsid_topvillage);
 
 //Daily Count
-app.get("/gettotaldc",dash_chart_dcfunc.report.get_total_dc);
+app.get("/gettotaldc", cor(), dash_chart_dcfunc.report.get_total_dc);
 app.post("/getdc_total_hwc_bydate",dash_chart_dcfunc.report.get_dc_bydate);
 
 //Publicity
-app.get("/getpublicity_total",dash_chart_pubfunc.report.get_publicity_total);
-app.get("/getpublicity_all",dash_chart_pubfunc.report.get_publicity);
-app.post("/getpublicity_bydate",dash_chart_pubfunc.report.get_pub_bydate);
+app.get("/getpublicity_total", cor(), dash_chart_pubfunc.report.get_publicity_total);
+app.get("/getpublicity_all", cor(), dash_chart_pubfunc.report.get_publicity);
+app.post("/getpublicity_bydate", cor(), dash_chart_pubfunc.report.get_pub_bydate);
 
 //Data Sync
 app.get("/syncdata",function(req,res){
