f1f305b8f664cc668f9b2a4c0dc0bc06d5e0ca82
kartimanu <karthik.manjunath@kaplan.com>
2018-11-29 17:55:39 +0530

    origin fix - 0.8

--- server.js
+++ server.js
@@ -28,22 +28,22 @@ var port = process.env.port || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 
-// app.use(function(req,res,next){
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     // Pass to next layer of middleware
-//     next();
-//     // res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-// });
+app.use(function(req,res,next){
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+    // res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+});
 // app.use(function (req, res, next) {
 //     if (req.url !== '/authUser' && req.url !== "/") {
 //         var token = req.headers['authorization'];
