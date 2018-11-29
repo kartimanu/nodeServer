6412cf5966a359231683f3df5754db2265614157
kartimanu <karthik.manjunath@kaplan.com>
2018-11-29 14:56:56 +0530

    origin fix - 0.6

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 app.use(function(req,res,next){
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://wildseveodk.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://wildseveodk.com, https://www.wildseveodk.com');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
